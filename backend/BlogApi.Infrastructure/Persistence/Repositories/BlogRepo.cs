8cbdaa32d7d63ded6f4c1638b711a1f05a567134
karunland <1harunkorkmaz@gmail.com>
2025-02-16 13:24:18 +0300

    s

--- backend/BlogApi.Infrastructure/Persistence/Repositories/BlogRepo.cs
+++ backend/BlogApi.Infrastructure/Persistence/Repositories/BlogRepo.cs
@@ -51,7 +51,7 @@ public class BlogRepo(
             x.User.ExternalProvider == ExternalProviderEnum.Google && x.User.FileUrl != null && x.User.FileUrl.StartsWith("http") ? x.User.FileUrl : baseSettings.BackendUrl + "/api/file/image/" + x.User.FileUrl,
             x.Category.Name,
             x.CategoryId,
-            context.Views.Count(x => x.BlogId == x.Id),
+            context.Views.Count(v => v.BlogId == x.Id),
             x.BlogStatusEnum,
             x.BlogStatusEnum.ToString(),
             baseSettings.BackendUrl + "/api/file/image/" + x.ImageUrl,
