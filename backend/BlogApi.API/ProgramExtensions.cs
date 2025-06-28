b34ed845bead1c5c71f1d2a0a2e5eb84c418c0e3
Harun Korkmaz <1harunkorkmaz@gmail.com>
2025-06-28 21:56:37 +0300

    s

--- backend/BlogApi.API/ProgramExtensions.cs
+++ backend/BlogApi.API/ProgramExtensions.cs
@@ -87,8 +87,8 @@ public static class ProgramExtensions
                 {
                     builder
                         .WithOrigins(
-                            "http://localhost:3000", 
-                            "http://localhost:3001", 
+                            "http://localhost:3000",
+                            "http://localhost:3001",
                             "http://127.0.0.1:3000",
                             "http://127.0.0.1:3001",
                             "http://localhost:5000",
